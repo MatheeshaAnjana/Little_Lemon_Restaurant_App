3269d76e9401193c0168d0e87803b920c4bb6a47
Matheesha Anjana <matheeshaanjana01@gmail.com>
2026-03-05 00:02:47 +0530

    First Commit

---
+++ little_lemon/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Little Lemon</h1>
+      <p>Enjoy our delicious Mediterranean cuisine!</p>
+    </div>
+  );
+}
+
+export default App;
